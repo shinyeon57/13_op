b08d05ecb5aa365a31970ffe6451cb138aa2152e
shinyeon57 <noeys57@sookmyung.ac.kr>
2019-11-26 15:28:29 +0900

    practice 01

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct student {
+	int ID;
+	char name[10];
+	double grade;	
+};
+int main(int argc, char *argv[]) {
+	struct student s1 = {24 , "각자이름", 4.3};
+	return 0;
+}
